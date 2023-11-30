b6ed65e8f2fd3e19547903e6a0f01530029974a7
Subro <57826642+Subroz@users.noreply.github.com>
2023-11-30 17:59:55 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,6 +6,6 @@ class Config(object):
     #MongoDB URL
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://3823:123@cluster0.ijh7g54.mongodb.net/?retryWrites=true&w=majority")
     #Port number for running server
-    PORT = os.environ.get("PORT", 5000)
+    PORT = os.environ.get("PORT", 80)
     #Bind address/IP, recommended to keep 0.0.0.0
     BIND_ADDRESS = os.environ.get("BIND_ADDRESS", "0.0.0.0")
